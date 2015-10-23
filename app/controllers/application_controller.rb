6dd53d80f990961f092c3e69a62278f96f0fdb2a
Artem Bolshakov <abolshakov@spbtv.com>
2015-10-23 11:30:30 +0300

    Reproduce the error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  caches_action :with_cache
+
+  def with_cache
+    render json: 'with_cache'
+  end
+
+  def without_cache
+    render json: 'without_cache'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  get 'with_cache' => 'application#with_cache'
+  get 'without_cache' => 'application#without_cache'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
